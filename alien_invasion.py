0ae6cbf7290dc5ede0aa48c8d31d21e167174f1a
paulo.junior <phjph@hotmail.com>
2022-05-31 19:37:24 -0300

    Espaçonave adicionada no jogo

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,13 +1,17 @@
 import sys
 import pygame
 from settings import Settings
+from ship import Ship
 
 def run_game():
     # Inicializa o jogo, as configurações e cria um objeto para a tela
     pygame.init()
     ai_settings = Settings()
-    screen = pygame.display.set_mode(ai_settings.screen_width, ai_settings.screen_height)
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
+
+    # Cria uma espaçonave
+    ship = Ship(screen)
 
     # Define a cor de fundo
     bg_color = (230, 230, 230)
@@ -18,6 +22,7 @@ def run_game():
             if event.type == pygame.QUIT:
                 sys.exit()
             screen.fill(ai_settings.bg_color)
+            ship.blitme()
             pygame.display.flip()
 
 
